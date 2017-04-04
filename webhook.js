991c82267d917553b325a21b601d3efe2ff023ab
Thanatkorn <thanatkorn@exzyteam.com>
2017-04-04 19:23:18 +0700

    Add User Info

--- webhook.js
+++ webhook.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+const PAGE_ACCESS_TOKEN = "EAAGDrZCh2JNkBAN6dNzHGt9ANbtZAZCYCCjYwZALkHZAQadZCQiFzMagX3rHDY2bcIuC4kb0RkL5PiGZCXojvH1arJlAApZBPtFVK4zPWDz1E8xwW9CQZAX5dqz2wD4OFPqjSCboZCrwQV5WvAObgye6ue1I6x1sPtUvyrYlaSIcbESQZDZD";
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -24,7 +26,7 @@ app.post('/webhook',(req,res) => {
         req.body.entry.forEach((entry)=> {
             entry.messaging.forEach((event)=>{
                 if(event.message && event.message.text){
-                    sendMessage(event);
+                    getUserInfo(event);
                 }
             });
         });
@@ -32,36 +34,40 @@ app.post('/webhook',(req,res) => {
     }
 });
 
-function sendMessage(event) {
+function sendMessage(event,user) {
     let sender = event.sender.id;
     let text = event.message.text;
 
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: 'EAAGDrZCh2JNkBAN6dNzHGt9ANbtZAZCYCCjYwZALkHZAQadZCQiFzMagX3rHDY2bcIuC4kb0RkL5PiGZCXojvH1arJlAApZBPtFVK4zPWDz1E8xwW9CQZAX5dqz2wD4OFPqjSCboZCrwQV5WvAObgye6ue1I6x1sPtUvyrYlaSIcbESQZDZD'},
+        qs: {access_token: PAGE_ACCESS_TOKEN},
         method: 'POST',
+        // json: {
+        //     recipient: {id: sender},
+        //     message: {attachment: {
+        //         type: "template",
+        //         payload:{
+        //             template_type: "button",
+        //             text: "Choose your link",
+        //             buttons: [
+        //                 {
+        //                     type: "web_url",
+        //                     url: "https://www.google.co.th/search?q="+text,
+        //                     title: "Google"
+        //                 },
+        //                 {
+        //                     type: "web_url",
+        //                     url: "https://www.youtube.com/results?search_query="+text,
+        //                     title: "Youtube"
+        //                 }
+        //             ]
+        //         }
+        //     }}
+        // }   
         json: {
             recipient: {id: sender},
-            message: {attachment: {
-                type: "template",
-                payload:{
-                    template_type: "button",
-                    text: "Choose your link",
-                    buttons: [
-                        {
-                            type: "web_url",
-                            url: "https://www.google.co.th/search?q="+text,
-                            title: "Google"
-                        },
-                        {
-                            type: "web_url",
-                            url: "https://www.youtube.com/results?search_query="+text,
-                            title: "Youtube"
-                        }
-                    ]
-                }
-            }}
-        }   
+            message: {text: user.first_name}
+        }
     },function (error, response){
         if(error){
             console.log('Error sending message: ',error);
@@ -70,3 +76,22 @@ function sendMessage(event) {
         }
     });
 }
+
+function getUserInfo(event) {
+    let sender = event.sender.id;
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/'+sender,
+        qs: {access_token: PAGE_ACCESS_TOKEN},
+        method: 'GET'  
+    },function (error, response){
+        if(error){
+            console.log('Error sending message: ',error);
+        }else if(response.body.error){
+            console.log('Error: ', response.body.error);
+        }
+        var user = JSON.parse(response.body);
+        sendMessage(event,user);
+    });
+}
+
